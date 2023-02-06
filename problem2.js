d864823277352622295676d721197e47a9aa0dcd
Himanshu <90437854+himanshugarg0007@users.noreply.github.com>
2023-02-06 09:48:05 +0530

    commit changes
    js 101 day 1

---
+++ problem2.js
@@ -0,0 +1,6 @@
+let number=10;
+console.log(number);
+number=12345;
+console.log(number);
+var a=10;
+const b=119;
---
+++ problem3.js
@@ -0,0 +1,9 @@
+let y=10;
+const aadhar=12345;
+let name="Goku";
+
+console.log(name);
+
+let b=24;
+let a= "24";
+console.log(typeof(a));
